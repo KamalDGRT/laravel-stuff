a6838ba726153bb3bec399dcc1f931444bbd93ab
KamalDGRT <kamaldgrt@gmail.com>
2021-05-26 19:56:44 +0530

    learn: create Controller, make an action

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post',[
+    PostController::class,
+    'index'
+]);
