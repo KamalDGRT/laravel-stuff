d7695e85c381d2a6ad22418274dc83acfbede624
KamalDGRT <kamaldgrt@gmail.com>
2021-05-29 19:43:09 +0530

    learn: add few fields for the table (migration)

--- database/migrations/2021_05_28_064954_create_post_table.php
+++ database/migrations/2021_05_28_064954_create_post_table.php
@@ -15,6 +15,8 @@ class CreatePostTable extends Migration
     {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
