ed1ee13c14a58e7606b3dbd6b32c1f4a5926dd1e
KamalDGRT <kamaldgrt@gmail.com>
2021-06-18 07:41:04 +0530

    Learn: how to rollback a migration
    php artisan migrate:rollback
    After making the changes,
    php artisan migrate

--- database/migrations/2021_05_28_064954_create_post_table.php
+++ database/migrations/2021_05_28_064954_create_post_table.php
@@ -16,7 +16,7 @@ class CreatePostTable extends Migration
         Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('body');
+            $table->text('content');
             $table->timestamps();
         });
     }
